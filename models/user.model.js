2dfeb73173bc2b7d48085c7cdadadd74322fd187
Antor <antor7080@gmail.com>
2023-05-26 23:18:43 +0600

    test

--- models/user.model.js
+++ models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     default: "student",
-    enum: ["admin", "student", 'superAdmin', 'teacher',],
+    enum: ["admin", "student", 'superAdmin', 'teacher', 'doctor', 'staff'],
   },
   isVerified: {
     type: Boolean,
@@ -73,7 +73,6 @@ const userSchema = new mongoose.Schema({
   },
   shift: {
     type: String,
-    required: true,
     enum: ["day", "evening"]
   }
 
